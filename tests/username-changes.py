3b0330af083a89e658f0deea8ea0ffa87b9df3b5
theartcher <jorisplayz@gmail.com>
2025-06-11 14:47:01 +0200

    Update integration test to include searchdb

--- tests/username-changes.py
+++ tests/username-changes.py
@@ -15,6 +15,7 @@ ACCOUNT_API_URL = f"https://131.189.152.107.nip.io/api/accounts/{USER_ID}/name"
 # Environment variables/secrets
 ACCOUNT_DB_CONNECTION_STRING = os.getenv("ACCOUNT_DB_CONNECTION_STRING", "")
 POSTS_DB_CONNECTION_STRING = os.getenv("POSTS_DB_CONNECTION_STRING", "")
+POST_SEARCH_DB_CONNECTION_STRING = os.getenv("POST_SEARCH_DB_CONNECTION_STRING", "")
 
 print("Running integration test for PATCH request to update username...")
 
@@ -29,8 +30,13 @@ assert response.status_code in (200, 204), (
     f"PATCH failed: {response.status_code} {response.text}"
 )
 
+print(f"Called {ACCOUNT_API_URL} with '{TEST_USERNAME}")
+print("Waiting for data changes, sleeping 5s...")
+
 # 2. Wait for eventual consistency
 time.sleep(5)  # Adjust as needed
+
+print("Validating changes...")
 
 # 3. Connect to AccountsDb and verify user exists
 accounts_db_conn = psycopg2.connect(ACCOUNT_DB_CONNECTION_STRING)
@@ -47,7 +53,17 @@ with posts_db_conn.cursor() as cursor:
     )
     assert cursor.fetchone(), "No post found with updated username in PostsDb"
 
-print("Integration test completed successfully.")
+# 5. Connect to PostsSearchDb and verify post author username updated
+posts_db_conn = psycopg2.connect(POST_SEARCH_DB_CONNECTION_STRING)
+with posts_db_conn.cursor() as cursor:
+    cursor.execute(
+        'SELECT 1 FROM "Posts" WHERE "AuthorId" = %s AND "AuthorName" = %s',
+        (USER_ID, TEST_USERNAME),
+    )
+    assert cursor.fetchone(), "No post found with updated username in PostSearchDB"
+
+print("Changes validated successfully.")
+print("Integration test completed successfully!")
 
 RESET_USERNAME = "Test123"
 reset_response = requests.patch(
@@ -60,5 +76,5 @@ assert reset_response.status_code in (200, 204), (
     f"Reset PATCH failed: {reset_response.status_code} {reset_response.text}"
 )
 
-print("Username reset successfully.")
-print("All tests passed successfully.")
+print(f"Username reset to default value '{RESET_USERNAME}' successfully!")
+print("All tests passed successfully!")
